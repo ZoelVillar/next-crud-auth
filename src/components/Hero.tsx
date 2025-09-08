aeefa282b57045949fc2e728e3040b58e01e292b
Zoel Villar <villar.zoel@gmail.com>
2025-09-07 21:16:27 -0300

    img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight, CirclePlay } from "lucide-react";
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -9,12 +10,12 @@ const Hero = () => {
       <div className="max-w-screen-xl w-full mx-auto grid lg:grid-cols-2 gap-12 px-6 py-12 lg:py-0">
         <div className="my-auto">
           <h1 className="mt-6 max-w-[17ch] text-4xl md:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold !leading-[1.2] tracking-tight">
-            Customized Shadcn UI Blocks & Components
+            CRUD Application with Next.js 15 and Prisma
           </h1>
           <p className="mt-6 max-w-[60ch] text-lg">
-            Explore a collection of Shadcn UI blocks and components, ready to
-            preview and copy. Streamline your development workflow with
-            easy-to-implement examples.
+            Build a full-featured CRUD application using the latest Next.js 15
+            features and Prisma ORM for seamless database management. Made by
+            Zoel Villar.
           </p>
           <div className="mt-12 flex items-center gap-4">
             <Button size="lg" className="rounded-full text-base">
@@ -29,7 +30,15 @@ const Hero = () => {
             </Button>
           </div>
         </div>
-        <div className="w-full aspect-video lg:aspect-auto lg:w-[1000px] lg:h-screen bg-accent rounded-xl lg:rounded-none" />
+        <div className="relative w-full aspect-video lg:aspect-auto lg:w-[1000px] lg:h-screen rounded-xl lg:rounded-none overflow-hidden">
+          <Image
+            src="/imghero.png"
+            alt="Hero image"
+            fill
+            className="object-cover"
+            sizes="(min-width:1024px) 1000px, 100vw"
+          />
+        </div>
       </div>
     </div>
   );
